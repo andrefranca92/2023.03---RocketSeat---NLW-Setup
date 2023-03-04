c6a24550b9345e92a098d81e82e7c0ff93560b05
André França <andrewf992@gmail.com>
2023-03-04 15:37:35 -0300

    Adding and configuring Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client"
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
 /**
  * Métodos HTTP
@@ -10,10 +12,21 @@ const app = Fastify();
  *  Patch: atualiza parte de um registro
  *  Delete: deleta um registro
  *  */ 
-app.get('/', () => {
-  return 'Hello world'
+app.get('/hello', async () => {
+
+  const habits = await prisma.habit.findMany({
+    // where: {
+    //   title: {
+    //     startsWith: "Beber"
+    //   }
+    // }
+  })
+
+  return habits
 })
 
 app.listen({
   port: 3333
+}).then(() => {
+  console.log('HTTP server running')
 })
