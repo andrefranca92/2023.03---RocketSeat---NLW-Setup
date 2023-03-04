7a8fbf2ab030d0117c40c1bb3de412aaea93f9e5
André França <andrewf992@gmail.com>
2023-03-04 15:17:44 -0300

    Configuring Fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.log("olá!");
+import Fastify from "fastify";
+
+const app = Fastify();
+
+/**
+ * Métodos HTTP
+ *  Get: obtém um registro
+ *  Post: cria um registro
+ *  Put: atualiza um registro completo
+ *  Patch: atualiza parte de um registro
+ *  Delete: deleta um registro
+ *  */ 
+app.get('/', () => {
+  return 'Hello world'
+})
+
+app.listen({
+  port: 3333
+})
